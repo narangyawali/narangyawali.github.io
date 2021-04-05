11278588e780dd065155558043b6fb0bd6c03068
Naran Gyawali <58903698+narangyawali@users.noreply.github.com>
2021-04-05 18:31:51 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+
+function dino(){
+
+    window.open("https://narangyawali.github.io/dino_game/");
+
+}
+
+
+function sub(){
+
+    window.open("https://narangyawali.github.io/subway/");
+}
+
+
+function scb(){
+
+    window.open("https://narangyawali.github.io/scrollbar-in-js/");
+
+}
+
+
